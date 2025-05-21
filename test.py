c692513978c2b1b71d1f3f688be8be971707e395
Cary Xiao <caryxiao@stanford.edu>
2025-05-20 20:40:53 -0700

    init, create test python file to try out openai key

---
+++ test.py
@@ -0,0 +1,18 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI(
+  api_key=os.getenv("OPENAI_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4.1-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": "write a haiku about ai"}
+  ]
+)
+
+print(completion.choices[0].message);
